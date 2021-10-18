81c755f9e8f35ba9c4dc697d44711926b810c300
IR <na@intelligentreach.com>
2021-10-18 08:00:02 +0100

    rearrange import for app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
+import { AppComponent } from "./app.component";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
-import { AppComponent } from "./app.component";
 import { ProductsModule } from "./products/products.module";
 
 @NgModule({
